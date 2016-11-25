0a76b4365d1e78e584177b79b4f7b5b6e3fe04db
Amit <Amit@LAPTOP-AC3SP1OG>
2016-11-25 23:24:36 +0530

    Initial commit

---
+++ InitialGit/src/com/initial/test/InitTest.java
@@ -0,0 +1,10 @@
+package com.initial.test;
+
+public class InitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test the commit");
+	}
+
+}
